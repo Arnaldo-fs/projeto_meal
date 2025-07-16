d3db0ad362006db6081109b61c2b2338672ccc26
Arnaldo Ferreira Silva <arnaldoferreirasilva2015@gmail.com>
2025-07-16 09:57:27 -0300

    add imagens, descrições das comidas, caracteristicas e rotas de telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,40 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key})
-    : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vamos Cozinhar?',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch()
-                .copyWith(
-                  primary: Colors.pink,
-                  secondary:
-                      Colors.amber,
+        title: 'Vamos Cozinhar? =)',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(
+            primary: Colors.pink,
+            secondary: Colors.amber,
+          ),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light()
+              .textTheme
+              .copyWith(
+                titleLarge: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
                 ),
-        canvasColor:
-            const Color.fromRGBO(
-              255,
-              254,
-              229,
-              1,
-            ),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light()
-            .textTheme
-            .copyWith(
-              titleLarge: const TextStyle(
-                fontSize: 20,
-                fontFamily:
-                    'RobotoCondensed',
               ),
-            ),
-      ),
-      home: const CategoriesScreen(),
-    );
+        ),
+        routes: {
+          AppRoutes.HOME: (ctx) =>
+              CategoriesScreen(),
+          AppRoutes.CATEGORIES_MEALS: (ctx) =>
+              CategoriesMealsScreen(),
+        });
   }
 }
